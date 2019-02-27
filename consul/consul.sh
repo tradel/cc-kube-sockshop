69d2cb79e0c0674dc294074ffcd2fad26ef27fe0
Todd Radel <todd@hashicorp.com>
2019-02-27 01:29:22 -0500

    Wait for pod to start

--- consul/consul.sh
+++ consul/consul.sh
@@ -3,7 +3,7 @@
 helm repo add consul https://consul-helm-charts.storage.googleapis.com
 helm install --name=consul consul/consul -f ./values.yaml
 
-kubectl port-forward service/consul-ui 8500:80 &
+kubectl port-forward service/consul-ui 8500:80 --pod-running-timeout=1m &
 
 echo ""
 echo -n "Your Consul UI is at: http://localhost:8500"
